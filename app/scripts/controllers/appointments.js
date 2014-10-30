ff72450c8e71b875d02e258ea2ce8a3947b3da76
Samson Quaye <snaquaye@gmail.com>
2014-10-30 15:34:22 +0100

    added checked in feature

--- app/scripts/controllers/appointments.js
+++ app/scripts/controllers/appointments.js
@@ -29,19 +29,25 @@ angular.module('viLoggedClientApp')
         templateUrl: 'views/appointments/form.html',
         controller: 'AppointmentFormCtrl'
       })
+      .state('visitor-check-in', {
+        parent: 'root.index',
+        url: '/appointments/:appointment_id/check-in',
+        templateUrl: 'views/appointments/check-in.html',
+        controller: 'CheckInCtrl'
+      })
   })
   .controller('AppointmentCtrl', function ($scope, appointmentService) {
-    appointmentService.getAllAppointments()
+    appointmentService.all()
       .then(function (response) {
         console.log(response);
         $scope.appointments = response;
       })
       .catch(function (reason) {
         console.log(reason)
-      })
+      });
   })
   .controller('AppointmentFormCtrl', function ($scope, $stateParams, $state, visitorService,
-                                               userService, appointmentService) {
+                                               userService, appointmentService, utility) {
     $scope.appointment = {};
     $scope.default = {};
 
@@ -50,7 +56,7 @@ angular.module('viLoggedClientApp')
         .then(function (response) {
           $scope.visitor = response;
         })
-        .catch(function(reason) {
+        .catch(function (reason) {
           console.log(reason);
         });
     }
@@ -60,28 +66,28 @@ angular.module('viLoggedClientApp')
         .then(function (response) {
           $scope.host = response;
         })
-        .catch(function(reason) {
+        .catch(function (reason) {
           console.log(reason);
         });
     }
 
     $scope.convertDateStringToObject = function () {
-      $scope.appointment.appointment_date = new Date($scope.appointment_date);
+      $scope.appointment.appointment_date = new Date($scope.appointment_date).toJSON();
     };
 
     $scope.refreshHostsList = function () {
       userService.all()
-        .then(function(response){
+        .then(function (response) {
           $scope.hosts = response;
         })
-        .catch(function(reason){
+        .catch(function (reason) {
           console.log(reason);
         });
     };
 
     $scope.refreshVisitorsList = function () {
       visitorService.all()
-        .then(function(response) {
+        .then(function (response) {
           $scope.visitors = response;
         })
         .catch(function (reason) {
@@ -98,6 +104,8 @@ angular.module('viLoggedClientApp')
         $scope.appointment.host = angular.copy($scope.host);
       }
 
+      $scope.appointment.label_code = utility.generateRandomInteger();
+
       appointmentService.save($scope.appointment)
         .then(function (response) {
           $scope.appointment = angular.copy($scope.default);
@@ -107,4 +115,79 @@ angular.module('viLoggedClientApp')
           console.log(reason);
         });
     };
-  });
+  })
+  .controller('CheckInCtrl', function ($scope, $state, $stateParams, $q,
+                                       visitorService, appointmentService, entranceService,
+                                       vehicleTypeConstant, notificationService, utility) {
+    $scope.appointment = {};
+    $scope.appointment.restricted_items = [{
+      item_code: '',
+      item_name: '',
+      item_type: ''
+    }];
+    $scope.default = {};
+    $scope.vehicleTypes = vehicleTypeConstant;
+
+    entranceService.all()
+      .then(function (response) {
+        $scope.entrance = response;
+      })
+      .catch(function (reason) {
+      });
+
+    appointmentService.get($stateParams.appointment_id)
+      .then(function (response) {
+        $scope.appointment = response;console.log($scope.appointment)
+        if (angular.isUndefined($scope.appointment.restricted_items)) {
+          $scope.appointment.restricted_items = [{
+            item_code: '',
+            item_name: '',
+            item_type: ''
+          }];
+        }
+      })
+      .catch(function (reason) {
+      });
+
+    $scope.checkItemScope = function() {
+      if ($scope.item === false) {
+        $scope.appointment.restricted_items = [];
+      }
+    };
+
+    $scope.addItem = function() {
+      if (!angular.isDefined($scope.item)) {
+        $scope.item = true;
+        return;
+      }
+
+      if ($scope.item ===  false) {
+        $scope.item = true;
+      }
+
+      $scope.appointment.restricted_items.push({
+        item_code: '',
+        item_name: '',
+        item_type: ''
+      });
+    };
+
+    $scope.removeItem = function (index) {
+      $scope.appointment.restricted_items.splice(index, 1);
+
+      if ($scope.appointment.restricted_items.length === 0) {
+        $scope.item = false;
+      }
+    };
+
+    $scope.checkVisitorIn = function () {
+      $scope.appointment.checked_in = utility.getDateTime();
+      appointmentService.save($scope.appointment)
+        .then(function (response) {
+          $scope.appointment = angular.copy($scope.default);
+          $state.go('appointments');
+        })
+        .catch(function (reason) {
+        });
+    };
+});
