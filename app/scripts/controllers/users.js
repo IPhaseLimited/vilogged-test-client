0fbb6c3df4af584716c0ef0d0b823a361a31d005
Samson Quaye <snaquaye@gmail.com>
2014-11-01 19:08:34 +0100

    user feature

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -14,10 +14,28 @@ angular.module('viLoggedClientApp')
         parent: 'root.index',
         url: '/profile',
         templateUrl: 'views/user/user-profile.html',
-        controller: 'userProfileCtrl'
+        controller: 'UserProfileCtrl'
       })
   })
-  .controller('userProfileCtrl', function($scope, $interval, userService, appointmentService) {
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('users', {
+        parent: 'root.index',
+        url: '/users',
+        templateUrl: 'views/user/index.html',
+        controller: 'UsersCtrl'
+      })
+  })
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('createUser', {
+        parent: 'root.index',
+        url: '/user/add',
+        templateUrl: 'views/user/widget-form.html',
+        controller: 'UserFormCtrl'
+      })
+  })
+  .controller('UserProfileCtrl', function($scope, $interval, userService, appointmentService) {
     $scope.currentUser = userService.user;
 
     appointmentService.getAppointmentsByUser($scope.currentUser)
@@ -47,10 +65,54 @@ angular.module('viLoggedClientApp')
         console.log(reason);
       });
   })
-  .controller('UsersCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('UsersCtrl', function ($scope, userService) {
+    userService.all()
+      .then(function(response) {
+        $scope.users = response;
+      })
+      .catch(function(reason) {
+        console.log(reason);
+      });
+
+    $scope.toggleActive = function (id) {
+      userService.toggleUserActivationStatus(id)
+        .then(function(response){
+          console.log(response);
+        });
+    };
+
+    $scope.deleteAccount = function (id) {
+      userService.remove(id)
+    }
+  })
+  .controller('UserFormCtrl', function ($scope, $state, userService, companyDepartmentsService) {
+    $scope.currentUser = userService.user;
+    $scope.user = {};
+
+    companyDepartmentsService.all()
+      .then(function (response) {
+        $scope.departments = response;
+      })
+      .catch(function (reason) {
+        console.log(reason);
+      });
+
+    if (!$scope.currentUser.is_superuser) {
+      $state.go("users");
+    }
+
+    $scope.createUserAccount = function () {
+      if ($scope.user.user_profile !== undefined || $scope.user.user_profile === null) {
+        $scope.user.user_profile.home_phone = $scope.user.user_profile.home_phone || null;
+        $scope.user.user_profile.work_phone = $scope.user.user_profile.work_phone || null;
+      }
+
+      userService.save($scope.user)
+        .then(function() {
+          $state.go("users");
+        })
+        .catch(function(reason) {
+          console.log(reason);
+        });
+    }
   });
--- app/scripts/services/user-service.js
+++ app/scripts/services/user-service.js
@@ -12,7 +12,7 @@ angular.module('viLoggedClientApp')
     // AngularJS will instantiate a singleton by calling "new" on this function
      function getAllUsers() {
       var deferred = $q.defer();
-      $http.get(config.api.backend+'/api/v1/users')
+      $http.get(config.api.backend+'/api/v1/users/')
       //$http.get(config.api.backend+'/scripts/fixtures/users.json')
         .success(function(users) {
           deferred.resolve(users)
@@ -26,17 +26,9 @@ angular.module('viLoggedClientApp')
 
     function getUser(id) {
       var deferred = $q.defer();
-      getAllUsers()
-        .then(function(users) {
-          var user = {};
-          var filtered = users
-            .filter(function(row) {
-              return parseInt(row.id) === parseInt(id);
-            });
-          if (filtered > 0) {
-            user = filtered[0];
-          }
-          deferred.resolve(user);
+      $http.get(config.api.backend+'/api/v1/user/'+id+'/')
+        .then(function(response) {
+          deferred.resolve(response.data);
         })
         .catch(function(reason) {
           deferred.reject(reason);
@@ -56,8 +48,47 @@ angular.module('viLoggedClientApp')
       return deferred.promise;
     }
 
+    function saveUserAccount(user) {
+      var deferred = $q.defer();
+      $http.post(config.api.backend+'/api/v1/user/', user)
+        .success(function(response) {
+          deferred.resolve(response);
+        })
+        .error(function(reason) {
+          deferred.reject(reason);
+        });
+      return deferred.promise;
+    }
+
+    function updateUserAccount(id, user) {
+      var deferred = $q.defer();
+      $http.put(config.api.backend+'/api/v1/user/'+id, user)
+        .success(function(response) {
+          deferred.resolve(response);
+        })
+        .error(function(reason) {
+          deferred.reject(reason);
+        });
+      return deferred.promise;
+    }
+
+    function toggleUserAccountActive(id) {
+      var deferred = $q.defer();
+      getUser(id)
+        .then(function (response) {
+          response.is_active = !response.is_active;
+          updateUserAccount(id, response);
+        })
+        .catch(function(reason) {
+          console.log(reason);
+        });
+      return deferred.promise;
+    }
+
     this.all = getAllUsers;
     this.get = getUser;
     this.currentUser = getCurrentUser;
+    this.save = saveUserAccount;
     this.user = $cookieStore.get('current-user');
+    this.toggleUserActivationStatus = toggleUserAccountActive;
   });
