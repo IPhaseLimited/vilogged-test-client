c9dbc7a13939ab5714e056f054bdae56bdee64e5
Musa Musa <musakunte@gmail.com>
2014-11-01 22:17:59 +0100

    started sync service and settings page

---
+++ app/scripts/services/settings-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name viLoggedClientApp.settingsService
+ * @description
+ * # settingsService
+ * Service in the viLoggedClientApp.
+ */
+angular.module('viLoggedClientApp')
+  .service('settingsService', function settingsService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ app/scripts/services/sync-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name viLoggedClientApp.syncService
+ * @description
+ * # syncService
+ * Service in the viLoggedClientApp.
+ */
+angular.module('viLoggedClientApp')
+  .service('syncService', function syncService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
