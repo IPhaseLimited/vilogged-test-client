dab8e3f6317fa3ce18fc35e9bc72d1cf0ed2650d
Musa Musa <musakunte@gmail.com>
2014-11-02 15:39:59 +0100

    started report service

---
+++ app/scripts/services/report-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name viLoggedClientApp.reportService
+ * @description
+ * # reportService
+ * Service in the viLoggedClientApp.
+ */
+angular.module('viLoggedClientApp')
+  .service('reportService', function reportService() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
