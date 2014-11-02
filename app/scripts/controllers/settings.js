6e3fb5cfab1ed34bc239c1c8599e9fa31f978cb2
Samson Quaye <snaquaye@gmail.com>
2014-11-02 15:09:42 +0100

    create authorization service

---
+++ app/scripts/controllers/settings.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name viLoggedClientApp.controller:SettingsCtrl
+ * @description
+ * # SettingsCtrl
+ * Controller of the viLoggedClientApp
+ */
+angular.module('viLoggedClientApp')
+  .controller('SettingsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
