f200f93612b150ebf20cf7a6588bccbd7ad94e4f
Samson Quaye <snaquaye@gmail.com>
2014-11-06 22:33:27 +0100

    settings controller

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -2,24 +2,49 @@
 
 /**
  * @ngdoc function
- * @name viLoggedClientApp.controller:SettingsCtrl
+ * @name viLoggedClientApp.controller:ConfigurationCtrl
  * @description
- * # SettingsCtrl
+ * # ConfigurationCtrl
  * Controller of the viLoggedClientApp
  */
 angular.module('viLoggedClientApp')
   .config(function($stateProvider) {
     $stateProvider
       .state('settings', {
-        parent: 'root.index',
-        url: '/settings',
-        data: {
-          label: 'System Settings'
-        },
-        templateUrl: 'views/settings/index.html',
-        controller: 'SettingsCtrl'
+        'parent': 'root.index',
+        'url': '/settings',
+        'templateUrl': '/views/settings/setting.html',
+        'controller': 'SettingFormCtrl'
       })
   })
-  .controller('SettingsCtrl', function ($scope) {
+  .controller('SettingFormCtrl', function ($scope) {
+    $scope.currentPage = 'server setting';
+    $scope.currentPageTemplateUrl = '/views/settings/server-setting.html';
 
+    $scope.settings = {
+      serverSetting: {},
+      databaseSetting: {},
+      systemSetting: {}
+    };
+
+    $scope.setCurrentPage = function(page) {
+      switch (page) {
+        case 'server setting':
+              $scope.currentPage = 'server setting';
+              $scope.currentPageTemplateUrl = '/views/settings/server-setting.html';
+              break;
+        case 'database setting':
+              $scope.currentPage = 'database setting';
+              $scope.currentPageTemplateUrl = '/views/settings/database-setting.html';
+              break;
+        case 'system setting':
+              $scope.currentPage = 'system setting';
+              $scope.currentPageTemplateUrl = '/views/settings/system-setting.html';
+              break;
+        default:
+              $scope.currentPage = 'server setting';
+              $scope.currentPageTemplateUrl = '/views/settings/server-setting.html';
+              break;
+      }
+    }
   });
