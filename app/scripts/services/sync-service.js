51cfcec9eb1d50b9ba5c934cedaec5fc5d008565
Musa Musa <musakunte@gmail.com>
2014-11-08 23:20:41 +0100

    added start db replication, added checks for db changes

--- app/scripts/services/sync-service.js
+++ app/scripts/services/sync-service.js
@@ -8,9 +8,9 @@
  * Service in the viLoggedClientApp.
  */
 angular.module('viLoggedClientApp')
-  .service('syncService', function syncService($http, pouchdb, config) {
+  .service('syncService', function syncService($q, $http, pouchdb, config, db, $cookieStore) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    var db = pouchdb.create('visitors');
+    /*var db = pouchdb.create('visitors');
     db.replicate.to(config.api.localDB+'/visitors', {live: true})
       .then(function(response) {
         console.log(response);
@@ -23,53 +23,112 @@ angular.module('viLoggedClientApp')
         console.log(response);
       }).catch(function(reason){
         console.log(reason);
-      });
+      });*/
 
-    var failCount = 0;
-
-    var replicate = {
-      to: function() {
-        var syncParams = {
-          continuous: true,
-          create_target: true,
-          source: config.api.couchDB+'/visitors',
-          target: 'visitors'
-        };
-        return $http.post(config.api.localDB+'/_replicate', syncParams)
-      },
-      from: function() {
-        var syncParams = {
-          continuous: true,
-          create_target: true,
-          target: config.api.couchDB+'/visitors',
-          source: 'visitors'
-        };
-        return $http.post(config.api.localDB+'/_replicate', syncParams);
+    function getDBChanges(db) {
+      var lastSeq = $cookieStore.get(db);
+      if (angular.isUndefined(lastSeq)) {
+        lastSeq = 1;
       }
-    };
+      var deferred = $q.defer();
+      var status = {
+        update: false,
+        lastSeq: lastSeq
+      };
+      $http.get(config.api.couchDB+'/'+db+'/_changes?since='+lastSeq)
+        .success(function(response) {
+          if (response.last_seq > lastSeq) {
+            $cookieStore.put(db, response.last_seq);
+            status.update = true;
+            status.lastSeq = response.last_seq;
+          } else {
+            status.update = false;
+          }
+          deferred.resolve(status);
+        })
+        .error(function(reason) {
+          deferred.reject(reason);
+        });
+      return deferred.promise;
+    }
 
-    replicate.to()
-      .success(function () {
-        replicate.from()
-          .success(function() {
+    function replicateTo(db) {
+      var deferred = $q.defer();
+      var syncParams = {
+        continuous: true,
+        create_target: true,
+        source: config.api.couchDB+'/'+db,
+        target: db
+      };
+      $http.post(config.api.localDB+'/_replicate', syncParams)
+        .success(function(response) {
+          deferred.resolve(response);
+        })
+        .error(function(reason) {
+          if (reason.error === 'db_not_found') {
+            createDB(db)
+              .success(function(response) {
+                deferred.resolve(response);
+                return replicateTo(db);
+              })
+              .error(function(reason) {
+                deferred.reject(reason);
+              });
+          } else {
+            deferred.reject(reason);
+          }
+        });
+
+      return deferred.promise;
+    }
+
+    function replicateFrom(db) {
+      var deferred = $q.defer();
+      var syncParams = {
+        continuous: true,
+        create_target: true,
+        target: config.api.couchDB+'/'+db,
+        source: db
+      };
+      $http.post(config.api.localDB+'/_replicate', syncParams)
+        .success(function(response) {
+          deferred.resolve(response);
+        })
+        .error(function(reason) {
+          if (reason.error === 'db_not_found') {
+            createDB(db)
+              .success(function(response) {
+                deferred.resolve(response);
+                return replicateFrom(db);
+              })
+              .error(function(reason) {
+                deferred.reject(reason);
+              });
+          } else {
+            deferred.reject(reason);
+          }
+        });
+
+      return deferred.promise;
+    }
+
+    function createDB(db) {
+      return  $http.put(config.api.localDB+'/'+db);
+    }
+
+    function startUpReplication() {
+      (Object.keys(db)).forEach(function(key) {
+        var DB_NAME = db[key];
+        replicateFrom(DB_NAME)
+          .then(function() {
 
           })
-          .error(function(reason) {
-            failCount += 1;
-            if (failCount < 5) {
-              replicate.from();
-            }
-            console.log(reason);
+          .catch(function(reason) {
+
           });
-
-      })
-      .error(function(reason) {
-        failCount += 1;
-        if (failCount < 5) {
-          replicate.to();
-        }
-        console.log(reason);
       });
+    }
 
-
+    this.startReplication = startUpReplication;
+    this.getChanges = getDBChanges;
   });
