f38cbe6bb61d48822e185c3d238442c92ba39181
Musa Musa <musakunte@gmail.com>
2014-11-08 15:51:45 +0100

    created couchDB service

---
+++ app/scripts/services/couch-db-service.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name viLoggedClientApp.couchDbService
+ * @description
+ * # couchDbService
+ * Service in the viLoggedClientApp.
+ */
+angular.module('viLoggedClientApp')
+  .service('couchDbService', function couchDbService($resource, config) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+
+    var connect = $resource(config.api.couchDB + ':_db/:_action/:_param/:_sub/:_sub_param',
+      {
+        _db: '@_db'
+      },
+      {
+        allDocs: {
+          method: 'GET',
+          withCredentials: true,
+          params: {
+            _action: '_all_docs',
+            include_docs: true
+          }
+        },
+        bulkDocs: {
+          method: 'POST',
+          withCredentials: true,
+          params: {
+            _action: '_bulk_docs'
+          }
+        },
+        view: {
+          method: 'GET',
+          withCredentials: true,
+          params: {
+            _action: '_design',
+            _sub: '_view'
+          }
+        },
+        mapReduce: {
+          method: 'POST',
+          withCredentials: true,
+          params: {
+            _action: '_temp_view'
+          }
+        },
+        createDB: {
+          method: 'PUT',
+          withCredentials: true
+        },
+        deleteDB: {
+          method: 'DELETE',
+          withCredentials: true
+        },
+        session: {
+          method: 'GET',
+          withCredentials: true,
+          params: {
+            _db: '_session'
+          }
+        },
+        login: {
+          method: 'POST',
+          withCredentials: true,
+          params: {
+            _db: '_session'
+          }
+        },
+        logout: {
+          method: 'DELETE',
+          withCredentials: true,
+          params: {
+            _db: '_session'
+          }
+        }
+      })
+  });
