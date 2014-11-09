070442590b315101df527e7fa49e6e48b4d099ee
Musa Musa <musakunte@gmail.com>
2014-11-09 03:12:12 +0100

    started couch db factory

---
+++ app/scripts/services/couch-db-factory.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name viLoggedClientApp.couchDbFactory
+ * @description
+ * # couchDbFactory
+ * Factory in the viLoggedClientApp.
+ */
+angular.module('viLoggedClientApp')
+  .factory('couchDbFactory', function ($resource, config) {
+    return $resource(config.api.localDB +'/'+ ':_db/:_action/:_param/:_sub/:_sub_param',
+      {
+        _db: '@_db'
+      },
+      {
+        allDocs: {
+          method: 'GET',
+          withCredentials: true,
+          params: {
+            _action: '_all_docs',
+            include_docs: true
+          }
+        },
+        bulkDocs: {
+          method: 'POST',
+          withCredentials: true,
+          params: {
+            _action: '_bulk_docs'
+          }
+        },
+        put: {
+          method: 'PUT',
+          withCredentials: true
+        },
+        get: {
+          method: 'GET',
+          withCredentials: true
+        },
+        remove: {
+          method: 'DELETE',
+          withCredentials: true
+        },
+        view: {
+          method: 'GET',
+          withCredentials: true,
+          params: {
+            _action: '_design',
+            _sub: '_view'
+          }
+        },
+        mapReduce: {
+          method: 'POST',
+          withCredentials: true,
+          params: {
+            _action: '_temp_view'
+          }
+        },
+        createDB: {
+          method: 'PUT',
+          withCredentials: true
+        },
+        deleteDB: {
+          method: 'DELETE',
+          withCredentials: true
+        },
+        session: {
+          method: 'GET',
+          withCredentials: true,
+          params: {
+            _db: '_session'
+          }
+        },
+        login: {
+          method: 'POST',
+          withCredentials: true,
+          params: {
+            _db: '_session'
+          }
+        },
+        logout: {
+          method: 'DELETE',
+          withCredentials: true,
+          params: {
+            _db: '_session'
+          }
+        }
+      })
+  });
