1a278e7aa238b35d9a601cfc7681a176bf8d0484
Musa Musa <musakunte@gmail.com>
2014-11-09 03:07:32 +0100

    added sync to departments

--- app/scripts/services/company-departments-service.js
+++ app/scripts/services/company-departments-service.js
@@ -8,9 +8,13 @@
  * Service in the viLoggedClientApp.
  */
 angular.module('viLoggedClientApp')
-  .service('companyDepartmentsService', function companyDepartmentsService($q, storageService, db) {
+  .service('companyDepartmentsService', function companyDepartmentsService($q, storageService, db, syncService) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     var DB_NAME = db.COMPANY_DEPARTMENTS;
+
+    this.changes = function() {
+      return syncService.getChanges(DB_NAME);
+    };
 
     this.save = function(object) {
       return storageService.save(DB_NAME, object);
