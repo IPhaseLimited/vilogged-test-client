09c74661477a7c256678c987ec119c15752edf62
Musa Musa <musakunte@gmail.com>
2014-11-17 22:58:06 +0100

    added get all as object

--- app/scripts/services/company-departments-service.js
+++ app/scripts/services/company-departments-service.js
@@ -8,9 +8,26 @@
  * Service in the viLoggedClientApp.
  */
 angular.module('viLoggedClientApp')
-  .service('companyDepartmentsService', function companyDepartmentsService($q, storageService, db, syncService) {
+  .service('companyDepartmentsService', function companyDepartmentsService($q, storageService, db, utility, syncService) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     var DB_NAME = db.COMPANY_DEPARTMENTS.replace(/_/, '-');
+
+    function getAll() {
+      return storageService.all(DB_NAME);
+    }
+
+    function getAllObject() {
+      var deferred = $q.defer();
+      getAll()
+        .then(function(response) {
+          deferred.resolve(utility.castArrayToObject(response));
+        })
+        .catch(function(reason) {
+          deferred.reject(reason);
+        });
+
+      return deferred.promise;
+    }
 
     this.changes = function() {
       return syncService.getChanges(DB_NAME);
@@ -20,15 +37,16 @@ angular.module('viLoggedClientApp')
       return storageService.save(DB_NAME, object);
     };
 
-    this.all = function() {
-      return storageService.all(DB_NAME);
-    };
-
     this.get = function(id) {
+
       return storageService.find(DB_NAME, id);
     };
 
     this.remove = function(id) {
       return storageService.removeRecord(DB_NAME, id);
-    }
+    };
+
+    this.all = getAll;
+
+
   });
