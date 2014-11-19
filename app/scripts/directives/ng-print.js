7d0382c93d706eb3046355b84c688565efb5238c
Musa Musa <musakunte@gmail.com>
2014-11-19 10:16:52 +0100

    fixed minor bugs

--- app/scripts/directives/ng-print.js
+++ app/scripts/directives/ng-print.js
@@ -7,43 +7,35 @@
  * # ngPrint
  */
 angular.module('viLoggedClientApp')
-  .directive('ngPrint', function ($http, $compile, $timeout) {
+  .directive('ngPrint', function () {
 
     var printSection = document.getElementById('printSection');
-
     // if there is no printing section, create one
     if (!printSection) {
       printSection = document.createElement('div');
       printSection.id = 'printSection';
       document.body.appendChild(printSection);
     }
-
     function link(scope, element, attrs) {
       element.on('click', function () {
         var elemToPrint = document.getElementById(attrs.printElementId);
-
         if (elemToPrint) {
           printElement(elemToPrint);
-          window.print();
         }
       });
-
       window.onafterprint = function () {
         // clean the print section before adding new content
         printSection.innerHTML = '';
       }
     }
-
     function printElement(elem) {
       // clones the element you want to print
       var domClone = elem.cloneNode(true);
       printSection.appendChild(domClone);
+      window.print();
     }
-
-
     return {
       restrict: 'A',
-      scope: { obj: '=' },
       link: link
     };
   });
