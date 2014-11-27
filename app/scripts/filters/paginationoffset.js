89edca44da626cb64216de37ed65ed5066e71644
Musa Musa <musakunte@gmail.com>
2014-11-27 22:52:49 +0100

    removed console logs

--- app/scripts/filters/paginationoffset.js
+++ app/scripts/filters/paginationoffset.js
@@ -13,7 +13,6 @@ angular.module('viLoggedClientApp')
     return function (input, currentPage, itemsPerPage) {
       if (angular.isArray(input) && angular.isNumber(currentPage) && angular.isNumber(itemsPerPage)) {
         var startIndex = (currentPage - 1) * itemsPerPage;
-        console.log(startIndex);
         if (input.length < startIndex) {
           return [];
         } else {
